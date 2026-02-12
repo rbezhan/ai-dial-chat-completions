bc38a92113d8842c95ea6e6c444d7a3ca20c752b
Roman Bezhan <roman_bezhan@epam.com>
2026-02-12 14:36:05 +0300

    created client

--- task/clients/client.py
+++ task/clients/client.py
@@ -10,6 +10,8 @@ class DialClient(BaseClient):
 
     def __init__(self, deployment_name: str):
         super().__init__(deployment_name)
+        self._client = Dial(base_url=DIAL_ENDPOINT, api_key=self._api_key,)
+        self._async_client = AsyncDial(base_url=DIAL_ENDPOINT, api_key=self._api_key,)
         #TODO:
         # Documentation: https://pypi.org/project/aidial-client/
         # 1. Create Dial client with:
@@ -20,6 +22,16 @@ class DialClient(BaseClient):
         #   - api_key=self._api_key
 
     def get_completion(self, messages: list[Message]) -> Message:
+        response = self._client.chat.completions.create(
+            deployment_name=self._deployment_name,
+            messages=[msg.to_dict() for msg in messages],
+        )
+        if choices := response.get('choices'):
+            if message := choices[0].get('message'):
+                print(message['content'])
+                return Message(role=Role.ASSISTANT, content=message['content'])
+        
+        raise Exception("No choices in response found")
         #TODO:
         # 1. Create chat completions with client (client.chat.completions.create) with such params:
         #   - deployment_name=self._deployment_name
@@ -31,6 +43,21 @@ class DialClient(BaseClient):
         raise NotImplementedError
 
     async def stream_completion(self, messages: list[Message]) -> Message:
+        chunks = await self._async_client.chat.completions.create(
+            deployment_name=self._deployment_name,
+            messages=[msg.to_dict() for msg in messages],
+            stream=True,
+        )
+        contents = []
+        async for chunk in chunks:
+            if chunk.choices and len(chunk.choices) > 0:
+                delta = chunk.choices[0].delta
+                if delta and delta.content:
+                    print(delta.content, end='')
+                    contents.append(delta.content)
+        print()
+        return Message(role=Role.ASSISTANT, content=''.join(contents))  
+
         #TODO:
         # 1. Create chat completions with client (async_client.chat.completions.create) with such params:
         #   - deployment_name=self._deployment_name
