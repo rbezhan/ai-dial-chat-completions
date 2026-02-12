b867f7168db45954121dd8e09142701348ab5108
Roman Bezhan <roman_bezhan@epam.com>
2026-02-12 14:53:44 +0300

    Implemeted app

--- task/app.py
+++ task/app.py
@@ -8,6 +8,42 @@ from task.models.role import Role
 
 
 async def start(stream: bool) -> None:
+    client = DialClient(
+        deployment_name='gpt-4o',
+    )
+    custom_client = DialClient(
+        deployment_name='gpt-4o',
+    )
+    conversation = Conversation()
+    print("Enter system prompt (or press Enter to use default): ")
+    system_prompt = input("> ").strip()
+    
+    if system_prompt:
+        conversation.add_message(Message(role=Role.SYSTEM, content=system_prompt))
+        print("System prompt successfully added to conversation.")
+    else:
+        conversation.add_message(Message(role=Role.SYSTEM, content=DEFAULT_SYSTEM_PROMPT))
+        print(f"No System prompt provided. So, default system prompt added to conversation: {DEFAULT_SYSTEM_PROMPT}")
+    print()
+
+    print("Type your question or 'exit' to end the conversation.")
+    while True:
+        user_input = input("> ").strip()
+
+        if user_input.lower() == 'exit':
+            print("Exiting the conversation. Bye!")
+            break
+        
+        conversation.add_message(Message(role=Role.USER, content=user_input))
+
+        print("AI:")
+        if stream:
+            assistant_message = await client.stream_completion(conversation.get_messages())
+        else:
+            assistant_message = custom_client.get_completion(conversation.get_messages())
+        
+        conversation.add_message(assistant_message)
+    
     #TODO:
     # 1.1. Create DialClient
     #    - deployment_name: available gpt model. Sample: `gpt-4o`
